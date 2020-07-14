9b95e9d3f14f02cfc1bb7aaf7831d94e363bf2a3
Gextox <gabriel_il@live.com>
2020-07-14 20:26:44 -0300

    Updates

---
+++ precache-manifest.10b974c7defda59ee0e92a21c5c219bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ea8ca2cd67658fcbb188dd462a006f",
+    "url": "/Lyrics-App/index.html"
+  },
+  {
+    "revision": "21ecc2fdb20cccb41e7e",
+    "url": "/Lyrics-App/static/css/main.3ee45b84.chunk.css"
+  },
+  {
+    "revision": "e5d0c1e9caf32c709b05",
+    "url": "/Lyrics-App/static/js/2.c1ece7d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lyrics-App/static/js/2.c1ece7d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ecc2fdb20cccb41e7e",
+    "url": "/Lyrics-App/static/js/main.370eb9cf.chunk.js"
+  },
+  {
+    "revision": "86df11439661706b0dd6",
+    "url": "/Lyrics-App/static/js/runtime-main.c9629c24.js"
+  }
+]);
